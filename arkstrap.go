47735321906eaf07aa19696e915b413a6267ddf5
Alec Scott <alec@bcs.sh>
2021-08-05 22:36:11 -0700

    Initial Commit of Bootstrap Node

---
+++ arkstrap.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/arken/arkstrap/config"
+	"github.com/arken/arkstrap/ipfs"
+)
+
+func main() {
+	node, err := ipfs.CreateNode(config.Global.Ipfs.Path, ipfs.NodeConfArgs{
+		Addr:           config.Global.Ipfs.Addr,
+		PeerID:         config.Global.Ipfs.PeerID,
+		PrivKey:        config.Global.Ipfs.PrivateKey,
+		SwarmKey:       config.Manifest.ClusterKey,
+		BootstrapPeers: config.Manifest.BootstrapPeers,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print Node ID
+	fmt.Printf("ID: %s\n", node.ID())
+	// Wait for a SIGINT or SIGTERM signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	<-ch
+	fmt.Println("Received signal, shutting down...")
+	os.Exit(0)
+}
